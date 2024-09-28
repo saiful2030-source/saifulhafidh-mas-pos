f4e40faf84260b0fc4205642e76045652dca2864
root <root@srv528698.hstgr.cloud>
2024-09-28 04:30:22 +0000

    change default usage

--- README.md
+++ README.md
@@ -10,23 +10,33 @@
 ### How to Run the Application
 1. Clone this repository <code>git clone https://github.com/naufal-rafif/laravel-filament-starter.git appname</code>
 2. Run <code>composer install</code>
-2. Run <code>php artisan migrate --seed</code>
+3. Make sure your xampp mysql and server is run.
+4. Run <code>php artisan migrate --seed</code>
+5. Run <code>php artisan serve</code>
 
 ## Docker Usage
 
+Change environment and 
+```
+cp .env.example .env
+cp docker-compose.yml.default docker-compose.yml
+```
+
 #### Development Environment
 ```
-sh stub/local/setup.sh your-docker-container-name
+sh stub/local/setup.sh
 ```
 
 #### Production Environment
 ```
-sh stub/prod/setup.sh your-docker-container-name
+sh stub/prod/setup.sh
 ```
 
 ### Docker Usage Note
 - On development we use npm package **chokidar** to update change when reload. You can remove `--watch` on **supervisord.conf** or you can choose setup on production mode
 - We use Laravel Octane with frankenphp server, you can change or remove it if you don't want use it
+- Please consider use default container name logic on docker-compose.yml to run bash script (It use **COINTAINER_NAME** variable on .env file)
+- It's free to change database like mysql, mariadb, postgres, etc. But we just use Postgres in this starter kit for example.
 
 ## Filament Cheat Sheet
 
--- stub/local/setup.sh
+++ stub/local/setup.sh
@@ -1,21 +1,18 @@
-APP_NAME="${1:-app-name}"
+CONTAINER_NAME=$(grep ^CONTAINER_NAME= .env | cut -d '=' -f2 | tr -d '"')
 
-cp .env.example .env
-sed -i "s/^CONTAINER_NAME=.*/CONTAINER_NAME=$APP_NAME/" .env
-cp stub/local/docker-compose.yml.default docker-compose.yml
-
+CONTAINER_NAME=${CONTAINER_NAME:-starter-project}
 docker compose up -d
 
-docker exec $APP_NAME composer install
+docker exec $CONTAINER_NAME composer install
 
-docker exec $APP_NAME chmod -R ugo+rw vendor/
-docker exec $APP_NAME chmod -R ugo+rw bootstrap/cache/
-docker exec $APP_NAME chmod -R ugo+rw storage/
+docker exec $CONTAINER_NAME chmod -R ugo+rw vendor/
+docker exec $CONTAINER_NAME chmod -R ugo+rw bootstrap/cache/
+docker exec $CONTAINER_NAME chmod -R ugo+rw storage/
 
-docker exec $APP_NAME chmod ugo+rw composer.lock
-docker exec $APP_NAME chmod ugo+rw composer.json
+docker exec $CONTAINER_NAME chmod ugo+rw composer.lock
+docker exec $CONTAINER_NAME chmod ugo+rw composer.json
 
-docker exec $APP_NAME php artisan migrate --seed
-docker exec $APP_NAME npm install chokidar
-docker exec $APP_NAME cp stub/local/frankenphp frankenphp
-docker exec $APP_NAME php artisan octane:install
+docker exec $CONTAINER_NAME php artisan migrate --seed
+docker exec $CONTAINER_NAME npm install chokidar
+docker exec $CONTAINER_NAME cp stub/local/frankenphp frankenphp
+docker exec $CONTAINER_NAME php artisan octane:install
